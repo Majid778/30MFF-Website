187f6cef1f97fad69a02673b9168cdaaea0113c3
Majid778 <majidibrahim778@gmail.com>
2023-02-12 19:13:37 +0400

    final commit

---
+++ README.md
@@ -0,0 +1,17 @@
+
+
+Project Name: Similarity
+
+Project Description: 
+
+The project was to create a 15-minute video on any topic of our choosing, followed by the creation of this website. I (Majid) and Abraiz constituted the team for the project. During a brainstorming session, we saw an amazing wall of posters that gave us the idea for this movie. Each sentence on the posters displayed in the video evoked a mental image, either figurative or actual. The challenge in creating this film was finding creative ways to approximate the meaning of the phrases on the posters. I planned for the site to have a minimalist appeal with not too much going on. In addition, I wanted to add a section to showcase my other works.
+
+Process: 
+
+In terms of making the site, I experimented with different color palettes, including the one in my wireframe, and decided that going for black and white with a red accent was most aesthetically pleasing. The site structure in HTML is simple, with multiple cards aligned in a column format. Each card contains different information about the project. Within each card are different components that vary based on what I want to display there. This was done to give the site a clean, simple look, like how Apple designs its sites. I also added some javascript functions, which include a video catalog that you can scroll through by clicking on the arrow buttons, as well as a button at the bottom of the page that takes you back to the top for ease of access.
+
+Reflection/Evaluation: 
+
+I'm happy with how the website works technically, but I think the design could be better. The project's concept was to find similarities in various things, which I felt could have been better incorporated into the website's design, but I was short on time for this assignment, so I went with a simpler version.
+
+Before this project, I already knew some basic HTML and CSS. However, making this site and putting it online forced me to polish and improve my web design skills. I am also glad that I went out of my comfort zone and experimented with some simple JS, which I had never done before. Overall Overall, the project has met my expectations, helped me sharpen my  skills, and has been very enjoyable to work on.
--- script.js
+++ script.js
@@ -1,19 +1,25 @@
+
+// This is the main JavaScript file for the website.
 window.onscroll = function() {scrollFunction()};
 
+//This function is used to display the "Back to Top" button when the user scrolls down 800px from the top of the document
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
     document.getElementById("topBtn").style.display = "block";
   } else {
     document.getElementById("topBtn").style.display = "none";
   }
+
 }
 
+//This function is used to scroll to the top of the document when the user clicks on the "Back to Top" button
 document.getElementById("topBtn").addEventListener("click", function(){
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
 
-
+//video carousel for youtube iframes
 const videoContainers = document.querySelectorAll('.video-container');
 const prevBtn = document.querySelector('#prev-btn');
 const nextBtn = document.querySelector('#next-btn');
@@ -21,15 +27,31 @@ let currentIndex = 0;
 
 videoContainers[currentIndex].classList.add('active');
 
+//This function is used to display the previous slide when the user clicks on the "Previous arrow" button
 prevBtn.addEventListener('click', () => {
+  // let ifr be the iframe inside the current videoContainer
+  let ifr = videoContainers[currentIndex].querySelector('iframe');
+  // refresh src tag in the iframe to force stop the prev video
+  let src = ifr.src;
+  ifr.src = src;
   videoContainers[currentIndex].classList.remove('active');
   currentIndex = (currentIndex - 1 + videoContainers.length) % videoContainers.length;
   videoContainers[currentIndex].classList.add('active');
-});
-
+}
+);
+//This function is used to display the next slide when the user clicks on the "Next arrow" button
 nextBtn.addEventListener('click', () => {
+  // let ifr be the iframe inside the current videoContainer
+  let ifr = videoContainers[currentIndex].querySelector('iframe');
+  // refresh src tag in the iframe to force stop the prev video
+  let src = ifr.src;
+  ifr.src = src;
   videoContainers[currentIndex].classList.remove('active');
   currentIndex = (currentIndex + 1) % videoContainers.length;
   videoContainers[currentIndex].classList.add('active');
-});
+}
+);
+
+
+
 
